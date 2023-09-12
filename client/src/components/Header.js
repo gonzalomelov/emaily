5cf46123f571c4bbced8392c77d5766ff7fe6470
Gonzalo Melo <gonzalomelov@gmail.com>
2023-09-12 19:32:46 -0400

    First version of Redux Thunk

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,10 +3,19 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
-    )
+      <nav>
+        <div className="nav-wrapper">
+          <a href="/" className="left brand-logo">
+            Emaily
+          </a>
+          <ul className="right">
+            <li>
+              <a href="/auth/google">Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
