428107f24a6133add6d19c945783cdfed65edab6
Gonzalo Melo <gonzalomelov@gmail.com>
2023-09-13 09:45:21 -0400

    Wire up login/logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,18 +4,22 @@ module.exports = (app) => {
   app.get(
     '/auth/google',
     passport.authenticate('google', {
-      scope: ['profile', 'email']
+      scope: ['profile', 'email'],
     })
   );
-  
-  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => res.redirect('/')
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
-}
+};
