61800e2cbf3740facce6553a83214079b4ff41d7
Gonzalo Melo <gonzalomelov@gmail.com>
2023-09-26 10:37:39 -0400

    Format HTML

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -24,7 +24,10 @@ class SurveyForm extends Component {
           <Link to="/surveys" className="red btn-flat white-text">
             Cancel
           </Link>
-          <button type='submit' className='teal btn-flat right white-text'>Next<i className='material-icons right'>done</i></button>
+          <button type='submit' className='teal btn-flat right white-text'>
+            Next
+            <i className='material-icons right'>done</i>
+          </button>
         </form>
       </div>
     )
