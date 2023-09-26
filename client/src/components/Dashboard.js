7f51c041d9da2db9c0c61231ea08b5a4ad56c51d
Gonzalo Melo <gonzalomelov@gmail.com>
2023-09-26 11:27:30 -0400

    Fix class naming

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,12 +5,12 @@ const Dashboard = () => {
   return (
     <>
       <h1>Dashboard</h1>
-      <div class="fixed-action-btn">
+      <div className="fixed-action-btn">
         <Link
           to='/surveys/new'
           className="btn-floating btn-large red"
         >
-          <i class="large material-icons">add</i>
+          <i className="large material-icons">add</i>
         </Link>
       </div>
     </>
