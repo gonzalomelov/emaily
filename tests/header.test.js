75aecd3d233edb7a04bf40fc54237cd5861491b8
Gonzalo Melo <gonzalomelov@gmail.com>
2023-10-13 10:41:11 -0400

    Encapsulate augmented page logic into a class

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,31 +1,17 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
+const AuthenticablePage = require('./helpers/AuthenticablePage');
 
 const DOMAIN = 'http://localhost:3001';
 
-let browser, page;
-
-async function authenticate() {
-  const user = await userFactory();
-  const session = sessionFactory(user._id);
-  
-  await page.setCookie({ name: 'session', value: session.session });
-  await page.setCookie({ name: 'session.sig', value: session.sig });
-  await page.goto(DOMAIN);
-  await page.waitForSelector('a[href="/api/logout"]');
-}
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  });
-  page = await browser.newPage();
+  page = await AuthenticablePage.build();
+
   await page.goto(DOMAIN);
 })
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 })
 
 test('the header has the correct text', async () => {
@@ -42,8 +28,8 @@ test('clicking login starts OAuth flow', async () => {
   expect(url).toMatch(/accounts\.google\.com/);
 })
 
-test('when signed in, shows logout button', async () => {
-  await authenticate();
+test.only('when signed in, shows logout button', async () => {
+  await page.login();
 
   const text = await page.$eval('a[href="/api/logout"]', el => el.innerHTML);
 
