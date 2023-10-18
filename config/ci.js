f29f0227bf548722f19039b977721b2a102a2f74
Gonzalo Melo <gonzalomelov@gmail.com>
2023-10-18 16:24:59 -0400

    Add ci config

---
+++ config/ci.js
@@ -0,0 +1,13 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  redis: process.env.REDIS,
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridApiKey: process.env.SEND_GRID_API_KEY,
+  api: process.env.API,
+  web: process.env.WEB,
+  mailerFrom: process.env.MAILER_FROM,
+}
